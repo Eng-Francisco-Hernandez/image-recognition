631f9609965be5f516a7b4faaf5a2d380d33b423
Eng-Francisco-Hernandez <fjch87@gmail.com>
2023-05-05 01:18:45 -0600

    feat(image-recognition): made clean up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+import "@/styles/globals.scss";
 import "@ionic/react/css/core.css";
 /* Basic CSS for apps built with Ionic */
 import "@ionic/react/css/normalize.css";
